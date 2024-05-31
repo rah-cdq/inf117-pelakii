b322405594230a859ea0577031523791b8bc866a
rah-cdq <116538299+rah-cdq@users.noreply.github.com>
2024-05-31 15:16:11 -0700

    added tax settings no secret

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import address_ui
 import stripe
 import tkinter as tk
-stripe.api_key = ""
+
 
 if __name__ == "__main__":
   root = tk.Tk()
@@ -9,11 +9,11 @@ if __name__ == "__main__":
   root.mainloop()
 
   #stripe.tax.Settings.retrieve(stripe_account='{{CONNECTED_ACCOUNT_ID}}')
-  '''
+  
   stripe.tax.Settings.modify(
-  defaults={"tax_code": "txcd_10000000", "tax_behavior": "inclusive"},
-  head_office={"address": {"country": "DE"}},
-  )'''
+  defaults={"tax_code": "txcd_40070005", "tax_behavior": "inclusive"},
+  head_office={"address": {"line1":"390 NE 191st St #8129","city":"Miami","state":"FL","postal_code":"33179","country": "US"}},
+  )
 
   calc_1 = stripe.tax.Calculation.create(
     currency="usd",
@@ -29,4 +29,4 @@ if __name__ == "__main__":
       "address_source": "shipping",
     },
   )
-  print(calc_1.tax_amount_exclusive)
+  print(calc_1.tax_amount_inclusive)
