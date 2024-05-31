a20a0876d3b1ae3ef61305ad2b5538157de75ffb
rah-cdq <116538299+rah-cdq@users.noreply.github.com>
2024-05-31 14:21:39 -0700

    SECURITY

--- main.py
+++ main.py
@@ -9,12 +9,19 @@ if __name__ == "__main__":
   app = address_ui.UserAddress(root)
   root.mainloop()
 
+  #stripe.tax.Settings.retrieve(stripe_account='{{CONNECTED_ACCOUNT_ID}}')
 
   stripe.tax.Calculation.create(
-  currency="usd",
-  line_items=[{"amount": 1000, "reference": "L1"}],
-  customer_details={
-    "address": app.address_current,
-    "address_source": "shipping",
-  },
+    currency="usd",
+    line_items=[{"amount": 1000, "reference": "L1"}],
+    customer_details={
+      "address": {
+        "line1": app.address_current[0],
+        "city": app.address_current[1],
+        "state": app.address_current[2],
+        "postal_code": app.address_current[3],
+        "country": app.address_current[4],
+      },
+      "address_source": "shipping",
+    },
   )
